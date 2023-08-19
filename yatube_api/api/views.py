c5a343f3d4c94a861c5ef75dfb048dce7583e48a
PavelMinsky45 <Minsk5545@gmail.com>
2023-08-19 18:35:05 +0300

    dsfa

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
-from .mixins import UpdateDestroyMixin
 from posts.models import Post, Group
+
+
+class UpdateDestroyMixin:
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Нельзя редактировать, у тебя нет прав!')
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Нельзя удалять, у тебя нет прав!')
+        super().perform_destroy(instance)
 
 
 class PostViewSet(UpdateDestroyMixin, viewsets.ModelViewSet):
@@ -16,7 +28,6 @@ class PostViewSet(UpdateDestroyMixin, viewsets.ModelViewSet):
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
-
     serializer_class = GroupSerializer
 
 
@@ -24,7 +35,7 @@ class CommentViewSet(UpdateDestroyMixin, viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
     def get_post(self):
-        return get_object_or_404(Post, pk=self.kwargs['pk'])
+        return get_object_or_404(Post, id=self.kwargs['id'])
 
     def get_queryset(self):
         return self.get_post().comments
